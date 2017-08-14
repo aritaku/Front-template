d793b46ab3a9800820b84350817c913e47cc8d4f
aritaku <aritaku19@gmail.com>
2017-08-14 16:33:31 +0900

    add: template files & settings

---
+++ gulpfile.js
@@ -0,0 +1,68 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const ejs = require('gulp-ejs');
+const autoprefixer = require('gulp-autoprefixer');
+const uglify = require('gulp-uglify');
+const browser = require('browser-sync');
+const plumber = require('gulp-plumber');
+const rename = require('gulp-rename');
+const cleanCss = require('gulp-clean-css');
+const webpack = require('gulp-webpack');
+const pkg = require('./package.json');
+
+gulp.task('server', function () {
+  browser({
+    server: {
+      baseDir: ''
+    }
+  });
+});
+
+gulp.task('sass', function () {
+  gulp.src(['sass/*.scss'])
+      .pipe(plumber())
+      .pipe(sass())
+      .pipe(autoprefixer())
+      .pipe(gulp.dest('./css/'))
+      .pipe(browser.reload({
+        stream: true
+      }))
+});
+
+gulp.task('minify-css', ['sass'], function () {
+  gulp.src('css/*.css')
+      .pipe(plumber())
+      .pipe(cleanCss({ compatibility: 'ie8' }))
+      .pipe(rename({ suffix: '.min' }))
+      .pipe(gulp.dest('./css/min'))
+      .pipe(browser.reload({
+        stream: true
+      }))
+});
+
+gulp.task("ejs", function() {
+    gulp.src(["ejs/**/*.ejs","ejs/*.ejs"])
+        .pipe(plumber())
+        .pipe(ejs())
+        .pipe(rename({extname: '.html'}))
+        .pipe(gulp.dest("./js"))
+        .pipe(browser.reload({
+          stream:true
+        }))
+});
+
+gulp.task('minify-js', ['ejs'], function () {
+  gulp.src(['js/**/*.js', '!js/min/**/*.js'])
+      .pipe(plumber())
+      .pipe(uglify())
+      .pipe(gulp.dest('./js/min'))
+      .pipe(browser.reload({stream: true}))
+});
+
+gulp.task('build', ['minify-css', 'minify-js']);
+
+gulp.task('default', ['server'], function () {
+  gulp.watch('sass/**/*.scss', ['sass']);
+  gulp.watch(['js/**/*.js', '!js/min/**/*.js'], ['js']);
+  gulp.watch('ejs/**/*.ejs', ['ejs']);
+});
